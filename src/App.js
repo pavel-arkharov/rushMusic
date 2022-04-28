f8d09118d75be0116db840fa9bcd4727e509507c
Pavel Arkharov <parkharo@c2r1p12.hive.fi>
2022-04-28 14:34:55 +0300

    secrets?

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import {useEffect, useState} from 'react';
 
 function App() {
 
-	const CLIENT_ID = 'd3bec1c2ac244fb59650d7067352468a';
+	require('dotenv').config();
+
+	const CLIENT_ID = process.env.CLIENT_ID;
+	const CLIENT_SECRET = process.env.CLIENT_SECRET;
 	const REDIRECT_URI = window.location.href;
 	const AUTH_ENDPOINT = 'http://accounts.spotify.com/authorize';
 	const RESPONSE_TYPE = 'token';
@@ -13,7 +16,7 @@ function App() {
 
 	var SpotifyApi = new SpotifyWebApi({
 		clientId: CLIENT_ID,
-		clientSecret: 'b15f58d1819a40f896051636521f08a9',
+		clientSecret: CLIENT_SECRET,
 		redirectUri: REDIRECT_URI
 	});
 
